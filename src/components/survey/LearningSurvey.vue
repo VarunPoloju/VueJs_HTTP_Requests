6f9b79fbad070f5796c55d073f3379c6652f2ba8
varun <varunpoloju966@gmail.com>
2024-05-03 11:22:22 +0530

    modified files and sent first POST request to Firebase using fetch method

--- src/components/survey/LearningSurvey.vue
+++ src/components/survey/LearningSurvey.vue
@@ -46,7 +46,6 @@ export default {
       invalidInput: false,
     };
   },
-  emits: ['survey-submit'],
   methods: {
     submitSurvey() {
       if (this.enteredName === '' || !this.chosenRating) {
@@ -54,11 +53,17 @@ export default {
         return;
       }
       this.invalidInput = false;
+      fetch('https://vue-http-demo-c3177-default-rtdb.firebaseio.com/surveys.json', {
+        method : 'POST',
+        headers : {
+          'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify({
+          name : this.enteredName,
+          rating : this.chosenRating
+        }),
+      })
 
-      this.$emit('survey-submit', {
-        userName: this.enteredName,
-        rating: this.chosenRating,
-      });
 
       this.enteredName = '';
       this.chosenRating = null;
